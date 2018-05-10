34f151592fcdd10f8e64285802ea9a1ec3b5111c
dome4 <dominic.seitz@gmx.de>
2018-05-10 15:02:23 +0200

    fixed password issue

--- env/environment.example.js
+++ env/environment.example.js
@@ -1,6 +1,7 @@
 module.exports = {
     accountName: 'your-cosmosdb-account-name-goes-here',
     databaseName: 'admin',
+    // use node.js 3.0 password
     key: 'your-key-goes-here',
     port: 10255
 };
