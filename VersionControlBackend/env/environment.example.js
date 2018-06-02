e3a4f7fd968e6aa322a62d0c8d7ed688a84213c3
dome4 <dominic.seitz@gmx.de>
2018-06-02 15:30:18 +0200

    use dotenv instead of js files

--- VersionControlBackend/env/environment.example.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    ACCOUNT_NAME: 'your-cosmosdb-account-name-goes-here',
-    DATABASE_NAME: 'admin',
-    // use node.js 3.0 password
-    KEY: 'your-key-goes-here',
-    DB_PORT: 10255
-};
--- VersionControlBackend/mongo.ts
+++ VersionControlBackend/mongo.ts
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+var env = require('dotenv').load();   
 
 /**
 * Set to Node.js native promises
@@ -6,45 +7,8 @@ const mongoose = require('mongoose');
 */
 mongoose.Promise = global.Promise;
 
-// environment variables
-var accountName = null;
-var databaseName = null;
-var key = null;
-var db_port = null;
-
-/**
- * use environment variables or alternatively variables of environment.js file
- */
-try {
-    if (process.env.ACCOUNT_NAME && process.env.DATABASE_NAME && process.env.KEY && process.env.PORT) {
-        accountName = process.env.ACCOUNT_NAME;
-        databaseName = process.env.DATABASE_NAME;
-        key = process.env.KEY;
-        db_port = process.env.DB_PORT;
-    } else {
-
-        const env = require('./env/environment');
-
-        accountName = env.ACCOUNT_NAME;
-        databaseName = env.DATABASE_NAME;
-        key = env.KEY;
-        db_port = env.DB_PORT;
-    }
-} catch (e) {
-    // check if the environment.js file is missing
-    if (e instanceof Error && e.message.indexOf('Cannot find module') != -1) {
-        console.error('----------------------------------------------------------');
-        console.error('environment variables missing and environment.js not found or another error occured');
-        console.error(e.message);
-        console.error('----------------------------------------------------------');
-    } else {
-        throw e;
-    }
-}
-
-
 // create db connection strings
-const mongoUri = `mongodb://${accountName}:${key}@${accountName}.documents.azure.com:${db_port}/${databaseName}?ssl=true`;
+const mongoUri = `mongodb://${process.env.ACCOUNT_NAME}:${process.env.DB_KEY}@${process.env.ACCOUNT_NAME}.documents.azure.com:${process.env.DB_PORT}/${process.env.DATABASE_NAME}?ssl=true`;
 
 /**
  * connect to mongoDB
