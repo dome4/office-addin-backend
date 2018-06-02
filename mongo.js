80cdfa2284d729aa9dadfcc7de3a4750ccc26101
dome4 <dominic.seitz@gmx.de>
2018-06-02 11:37:42 +0200

    changed environment logic

--- mongo.js
+++ mongo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 /**
 * Set to Node.js native promises
 * Per http://mongoosejs.com/docs/promises.html
@@ -6,20 +7,55 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // environment variables
-const env = require('./env/environment');
-const accountName = process.env.ACCOUNT_NAME || env.ACCOUNT_NAME;
-const databaseName = process.env.DATABASE_NAME || env.DATABASE_NAME;
-const key = process.env.KEY || env.KEY;
-const port = process.env.PORT || env.PORT;
+var accountName = null;
+var databaseName = null;
+var key = null;
+var port = null;
 
-// eslint-disable-next-line max-len
+/**
+ * use environment variables or alternatively variables of environment.js file
+ */
+try {
+    if (process.env.ACCOUNT_NAME && process.env.DATABASE_NAME && process.env.KEY && process.env.PORT) {
+        accountName = process.env.ACCOUNT_NAME;
+        databaseName = process.env.DATABASE_NAME;
+        key = process.env.KEY;
+        port = process.env.PORT;
+    } else {
+
+        const env = require('./env/environment');
+
+        accountName = env.ACCOUNT_NAME;
+        databaseName = env.DATABASE_NAME;
+        key = env.KEY;
+        port = env.PORT;
+    }
+} catch (e) {
+    if (e instanceof Error && e.code === 'MODULE_NOT_FOUND') {
+        console.error('----------------------------------------------------------');
+        console.error('environment variables missing and environment.js not found');
+        console.error('----------------------------------------------------------');
+    } else {
+        throw e;
+    }
+}
+
+// create db connection strings
 const mongoUri = `mongodb://${accountName}:${key}@${accountName}.documents.azure.com:${port}/${databaseName}?ssl=true`;
 
+/**
+ * connect to mongoDB
+ * 
+ */
 function connect() {
     mongoose.set('debug', true);
     return mongoose.connect(mongoUri);
 }
 
+/**
+ * export connect function and mongoose object
+ *
+ */
 module.exports = {
     connect,
     mongoose
