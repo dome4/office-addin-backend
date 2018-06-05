314bae2148a87ddb5d701a331730b2f58b0db4b6
dome4 <dominic.seitz@gmx.de>
2018-06-05 14:24:33 +0200

    RequirementRelationController implemented

---
+++ VersionControlBackend/controllers/requirement/requirement-relation.controller.ts
@@ -0,0 +1,17 @@
+﻿import BaseController from '../base.controller';
+import { RequirementRelation } from '../../models/requirement/requirement-relation.model';
+
+/**
+ * 
+ * 
+ */
+export class RequirementRelationController extends BaseController {
+
+    /**
+     * call super constructor
+     * 
+     */
+    constructor() {
+        super(RequirementRelation, '_id');
+    }
+}
--- VersionControlBackend/routes.ts
+++ VersionControlBackend/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { RequirementController } from './controllers/requirement/requirement.controller';
 import { UserController } from './controllers/user.controller';
 import { RequirementTemplatePartController } from './controllers/requirement/requirement-template-part.controller';
+import { RequirementRelationController } from './controllers/requirement/requirement-relation.controller';
 
 /**
  * create router
@@ -27,6 +28,10 @@ try {
     // requirement template parts
     router.use('/requirement-part', new RequirementTemplatePartController().singularRoute());
     router.use('/requirement-parts', new RequirementTemplatePartController().pluralRoute());
+
+    // requirement relations
+    router.use('/requirement-relation', new RequirementRelationController().singularRoute());
+    router.use('/requirement-relations', new RequirementRelationController().pluralRoute());
 
     // users
     router.use('/user', new UserController().singularRoute());
