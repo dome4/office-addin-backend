316d6f7ce21970f663d41389ef761ce1f44efed9
dome4 <dominic.seitz@gmx.de>
2018-06-06 14:17:10 +0200

    requirement controller read and list overriden

--- VersionControlBackend/controllers/requirement/requirement.controller.ts
+++ VersionControlBackend/controllers/requirement/requirement.controller.ts
@@ -2,6 +2,12 @@
 import { Requirement } from '../../models/requirement/requirement.model';
 
 /**
+ * max number of results for list()
+ * 
+ */
+const MAX_RESULTS = 100;
+
+/**
  * 
  * 
  */
@@ -14,4 +20,66 @@ export class RequirementController extends BaseController {
     constructor() {
         super(Requirement, '_id');
     }
+
+    /*
+     * start
+     * override super methods
+     */
+
+
+    /**
+     * reads a model by it’s primary key
+     * 
+     * @param id Rrimary Key
+     * @param reponse HttpResponse
+     */
+    read(id, response) {
+        var filter = {};
+        filter[this.id] = id;
+
+        const docquery = Requirement.findOne(filter).populate('descriptionParts').populate('relations')
+        docquery
+            .exec()
+            .then((modelInstances) => {
+
+                if (modelInstances) {
+
+                    // if model exists
+                    response.status(200).json(modelInstances);
+                } else {
+
+                    // if model not exists
+                    response.status(404).json({ error: 'ressource not found' });
+                }
+
+            })
+            .catch(error => {
+                response.status(500).send(error);
+                return;
+            });
+    }
+
+    /**
+     * lists all models
+     * 
+     * @param response HttpResponse
+     */
+    list(response) {
+
+        const docquery = Requirement.find({}).limit(MAX_RESULTS).populate('descriptionParts').populate('relations');
+        docquery
+            .exec()
+            .then((modelInstances) => {
+                response.status(200).json(modelInstances);
+            })
+            .catch(error => {
+                response.status(500).send(error);
+                return;
+            });
+    }
+
+    /*
+     * end
+     * override super methods
+     */
 }
